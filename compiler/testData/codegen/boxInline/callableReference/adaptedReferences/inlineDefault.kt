9ed1c610176e655055473a0a48f8edceabf3fc96
Igor Chevdar <igor.chevdar@jetbrains.com>
2021-02-04 14:08:12 +0500

    Tests fixes

--- compiler/testData/codegen/boxInline/callableReference/adaptedReferences/inlineDefault.kt
+++ compiler/testData/codegen/boxInline/callableReference/adaptedReferences/inlineDefault.kt
@@ -4,10 +4,16 @@
 // WITH_RUNTIME
 // KJS_WITH_FULL_RUNTIME
 
+// FILE: 1.kt
+package test
+
 inline fun foo(mkString: () -> String): String =
         mkString()
 
 inline fun bar (xs: CharArray = charArrayOf('O','K')) =
         String(xs)
+
+// FILE: 2.kt
+import test.*
 
 fun box(): String = foo(::bar)
--- compiler/testData/codegen/boxInline/callableReference/adaptedReferences/inlineVararg.kt
+++ compiler/testData/codegen/boxInline/callableReference/adaptedReferences/inlineVararg.kt
@@ -4,11 +4,17 @@
 // WITH_RUNTIME
 // KJS_WITH_FULL_RUNTIME
 
+// FILE: 1.kt
+package test
+
 inline fun foo(mkString: (Char, Char) -> String): String =
         mkString('O','K')
 
 inline fun bar (vararg xs: Char) =
         String(xs)
+
+// FILE: 2.kt
+import test.*
 
 fun box(): String = foo(::bar)
 // -> { a, b -> bar(a, b) }
